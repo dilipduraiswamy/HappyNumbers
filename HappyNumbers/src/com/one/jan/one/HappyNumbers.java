5c42b4f4435db09e87d76c19975ad7f16551dda5
dilip.duraiswamy <dilip.duraiswamy@BLR-L1415-00727.actcorp.local>
2018-01-14 22:02:53 +0530

    generateHappyNumbers by limit range if limitTo is 10 it will return 1 7 10 13 19 23 28 31 32 44

--- HappyNumbers/src/com/one/jan/one/HappyNumbers.java
+++ HappyNumbers/src/com/one/jan/one/HappyNumbers.java
@@ -4,6 +4,7 @@
 package com.one.jan.one;
 
 import java.util.HashSet;
+import java.util.TreeSet;
 
 /**
  * @author dilip.duraiswamy
@@ -12,24 +13,42 @@ import java.util.HashSet;
 public class HappyNumbers {
 
 	/**
+	 * @param limitTo
+	 * @return sorted set of happy numbers
+	 * @Example if limitTo is 10 it will return 1 7 10 13 19 23 28 31 32 44
+	 */
+	public static TreeSet<Long> generateHappyNumbers(int limitTo) {
+		HashSet<Long> happyNumbers = new HashSet<>();
+		for (long num = 1, count = 0; count < limitTo; num++) {
+			if (is_happy_num(num)) {
+
+				happyNumbers.add(num);
+				count++;
+			}
+		}
+		TreeSet<Long> happyNumbersTreeSet = new TreeSet<Long>();
+		happyNumbersTreeSet.addAll(happyNumbers);
+		return happyNumbersTreeSet;
+	}
+
+	/**
 	 * @param num
 	 * @return true/false
 	 */
-	public static boolean happy_num(long num){
-	       long m = 0;
-	       int digit = 0;
-	       HashSet<Long> cycle = new HashSet<Long>();
-		   while(num != 1 && cycle.add(num)){
-	           m = 0;
-	           while(num > 0){
-	               digit = (int)(num % 10);
-	               m += digit*digit;
-	               num /= 10;
-	           }
-	           num = m;
-	       }
-	       return num == 1;
-	   }   
-	
+	public static boolean is_happy_num(long num) {
+		long m = 0;
+		int digit = 0;
+		HashSet<Long> cycle = new HashSet<Long>();
+		while (num != 1 && cycle.add(num)) {
+			m = 0;
+			while (num > 0) {
+				digit = (int) (num % 10);
+				m += digit * digit;
+				num /= 10;
+			}
+			num = m;
+		}
+		return num == 1;
+	}
 
 }
